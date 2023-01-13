45795479dbc715d52ddcb0e93e6fdbd11b27ce79
Ludovic Spina <74927851+ludovicspina@users.noreply.github.com>
2023-01-13 16:56:00 +0100

    result

---
+++ resources/views/result.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div style="font-size: 50px">
+    VOUS VOUS ETES FAIT AVOIR PAR BEKIM EJUPI AVEC LES ELEVES DU LYCEE PASTEUR MONT ROLAND !
+</div>
+
+</body>
+</html>
